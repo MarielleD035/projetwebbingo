43403a1cbb01055e4c5d6507c7f000604bbc4659
v <116445593+beieb@users.noreply.github.com>
2023-12-01 13:55:50 +0100

    création page admin base
    +install UX-chart

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,13 +7,25 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(
+        private ChartBuilderInterface $chartBuilder,
+    ){
+
+    }
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
+        // ...set chart data and options somehow
+
+        return $this->render('admin/my-dashboard.html.twig', [
+            'chart' => $chart,
+        ]);
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
