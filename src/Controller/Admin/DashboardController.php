bc817d7e50d6111eb4526f278a86e09f98e2f269
v <116445593+beieb@users.noreply.github.com>
2023-12-01 15:10:18 +0100

    ff la admin marche pas

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,9 +6,14 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
+use App\Entity\Users;
+use App\Form\UsersType;
+
+
 
 class DashboardController extends AbstractDashboardController
 {
@@ -20,12 +25,16 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
         $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
         // ...set chart data and options somehow
 
+        if (3 == $this->getUser()->getRole()){
         return $this->render('admin/my-dashboard.html.twig', [
             'chart' => $chart,
         ]);
+        }
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
