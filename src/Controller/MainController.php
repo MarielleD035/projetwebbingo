de2bee6d041f81bb0de5b6b175a6728b9e5ac545
v <116445593+beieb@users.noreply.github.com>
2023-12-03 20:19:55 +0100

    correction route
    ça marche avec le taf de marielle omg

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class MainController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
         ]);
     }
 }
--- src/Security/LoginformAutenticatorAuthenticator.php
+++ src/Security/LoginformAutenticatorAuthenticator.php
@@ -49,7 +49,7 @@ class LoginformAutenticatorAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        return new RedirectResponse($this->urlGenerator->generate('app_main'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
